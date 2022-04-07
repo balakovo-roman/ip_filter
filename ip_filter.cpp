969aa2a3256f8b86d7e1e2f57bce9bf8bdc95dc0
Lebedev Roman <lebedev64bal@yandex.ru>
2022-04-07 23:16:15 +0300

    Changed code style to Google

--- ip_filter.cpp
+++ ip_filter.cpp
@@ -3,36 +3,36 @@
 #include <iostream>
 #include <algorithm>
 
-int main([[maybe_unused]] int argc, [[maybe_unused]] char const* argv[]) {
+int main([[maybe_unused]] int argc, [[maybe_unused]] char const *argv[]) {
 
-    try {
-        ip_list ip_pool;
-        input_ip_list(std::cin, ip_pool);
-        ip_pool.shrink_to_fit();
+  try {
+	ip_list ip_pool;
+	input_ip_list(std::cin, ip_pool);
+	ip_pool.shrink_to_fit();
 
-        // TODO reverse lexicographically sort
-        std::sort(ip_pool.begin(), ip_pool.end(), std::greater<ip_address>());
+	// TODO reverse lexicographically sort
+	std::sort(ip_pool.begin(), ip_pool.end(), std::greater<ip_address>());
 
-        std::cout << ip_pool;
+	std::cout << ip_pool;
 
-        auto filter = [&ip_pool](const ip_byte&first_byte, auto&& ...params) {
-            filter_template_func(std::cout, ip_pool, first_byte, std::forward<decltype(params)>(params)...);
-        };
+	auto filter = [&ip_pool](const ip_byte &first_byte, auto &&...params) {
+	  filter_template_func(std::cout, ip_pool, first_byte, std::forward<decltype(params)>(params)...);
+	};
 
-        // TODO filter by first byte and output
-        filter(1);
+	// TODO filter by first byte and output
+	filter(1);
 
-        // TODO filter by first and second bytes and output
-        filter(46, 70);
+	// TODO filter by first and second bytes and output
+	filter(46, 70);
 
-        // TODO filter by any byte and output
-        auto filter_any = [&ip_pool](const ip_byte ipByte) {
-            filter_any_ip_impl(std::cout, ip_pool, ipByte);
-        };
-        filter_any(46);
-    }
-    catch (const std::exception& e) {
-        std::cerr << e.what() << std::endl;
-    }
-    return 0;
+	// TODO filter by any byte and output
+	auto filter_any = [&ip_pool](const ip_byte ipByte) {
+	  filter_any_ip_impl(std::cout, ip_pool, ipByte);
+	};
+	filter_any(46);
+  }
+  catch (const std::exception &e) {
+	std::cerr << e.what() << std::endl;
+  }
+  return 0;
 }
--- lib/lib.h
+++ lib/lib.h
@@ -3,32 +3,33 @@
 #include <vector>
 #include <array>
 #include <string>
+#include <algorithm>
 
 using ip_byte = unsigned char;
 using ip_address = std::array<ip_byte, 4>;
 using ip_list = std::vector<ip_address>;
 using ip_list_ci = ip_list::const_iterator;
 
-bool is_line_contains_ip(const std::string& line);
-void input_ip_list(std::istream& ostream, ip_list& ip_pool);
-std::ostream& operator<<(std::ostream& ostream, const ip_address& ipAddress);
-std::ostream& operator<<(std::ostream& ostream, const ip_list& ipList);
+bool is_line_contains_ip(const std::string &line);
+void input_ip_list(std::istream &ostream, ip_list &ip_pool);
+std::ostream &operator<<(std::ostream &ostream, const ip_address &ipAddress);
+std::ostream &operator<<(std::ostream &ostream, const ip_list &ipList);
 
 template<typename ...Args>
-void filter_template_func(std::ostream& ostream, const ip_list& ipList, const ip_byte& ipByte, Args&&... ip_bytes) {
-    auto ipAddress = ip_address{ ipByte, (ip_byte)ip_bytes... };
+void filter_template_func(std::ostream &ostream, const ip_list &ipList, const ip_byte &ipByte, Args &&... ip_bytes) {
+  auto ipAddress = ip_address{ipByte, (ip_byte)ip_bytes...};
 
-    auto interval = std::make_pair(ipList.cbegin(), ipList.end());
+  auto interval = std::make_pair(ipList.cbegin(), ipList.end());
 
-    for (size_t cnt = 0; cnt < (1 + sizeof...(ip_bytes)); ++cnt) {
-        interval = std::equal_range(interval.first, interval.second, ipAddress,
-            [cnt](const ip_address& lhs, const ip_address& rhs) {
-                return lhs.at(cnt) > rhs.at(cnt);
-            }
-        );
-    }
+  for (size_t cnt = 0; cnt < (1 + sizeof...(ip_bytes)); ++cnt) {
+	interval = std::equal_range(interval.first, interval.second, ipAddress,
+								[cnt](const ip_address &lhs, const ip_address &rhs) {
+								  return lhs.at(cnt) > rhs.at(cnt);
+								}
+	);
+  }
 
-    ostream << ip_list(interval.first, interval.second);
+  ostream << ip_list(interval.first, interval.second);
 }
 
-void filter_any_ip_impl(std::ostream& ostream, const ip_list &ip_pool, const ip_byte ipByte);
+void filter_any_ip_impl(std::ostream &ostream, const ip_list &ip_pool, const ip_byte ipByte);
