f900fb240dcd73df7b4be6d29bdf66fb427e82ed
Lebedev Roman <lebedev64bal@yandex.ru>
2022-04-09 19:34:12 +0300

    Cleanup code

--- ip_filter.cpp
+++ ip_filter.cpp
@@ -1,38 +1,10 @@
-﻿#include "lib.h"
-
+﻿#include <sstream>
 #include <iostream>
-#include <algorithm>
+#include "ipv4.h"
+
+using namespace ipv4;
 
 int main([[maybe_unused]] int argc, [[maybe_unused]] char const *argv[]) {
 
-  try {
-	ip_list ip_pool;
-	input_ip_list(std::cin, ip_pool);
-	ip_pool.shrink_to_fit();
-
-	// TODO reverse lexicographically sort
-	std::sort(ip_pool.begin(), ip_pool.end(), std::greater<ip_address>());
-
-	std::cout << ip_pool;
-
-	auto filter = [&ip_pool](const ip_byte &first_byte, auto &&...params) {
-	  filter_template_func(std::cout, ip_pool, first_byte, std::forward<decltype(params)>(params)...);
-	};
-
-	// TODO filter by first byte and output
-	filter(1);
-
-	// TODO filter by first and second bytes and output
-	filter(46, 70);
-
-	// TODO filter by any byte and output
-	auto filter_any = [&ip_pool](const ip_byte ipByte) {
-	  filter_any_ip_impl(std::cout, ip_pool, ipByte);
-	};
-	filter_any(46);
-  }
-  catch (const std::exception &e) {
-	std::cerr << e.what() << std::endl;
-  }
   return 0;
 }
