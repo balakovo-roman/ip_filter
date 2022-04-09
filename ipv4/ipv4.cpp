f55bca3765a97ed34c0400c36d291a2fce7cce71
Lebedev Roman <lebedev64bal@yandex.ru>
2022-04-09 19:31:43 +0300

    Added a simple class for processing IP addresses

---
+++ ipv4/ipv4.cpp
@@ -0,0 +1,33 @@
+#include "ipv4.h"
+
+#include <iostream>
+#include <sstream>
+
+namespace ipv4 {
+
+std::string Ipv4::to_string() const {
+  std::stringstream result;
+  result << *this;
+  return result.str();
+}
+
+std::ostream &operator<<(std::ostream &os, const Ipv4 &ipv_4) {
+  return os << static_cast<int>(ipv_4.data_[0]) << '.' << static_cast<int>(ipv_4.data_[1]) << '.'
+			<< static_cast<int>(ipv_4.data_[2]) << '.' << static_cast<int>(ipv_4.data_[3]);
+}
+std::istream &operator>>(std::istream &is, Ipv4 &ipv_4) {
+  char d1, d2, d3;
+  int b1, b2, b3, b4;
+
+  is >> b1 >> d1 >> b2 >> d2 >> b3 >> d3 >> b4;
+
+  if (d1 == '.' && d2 == '.' && d3 == '.')
+	ipv_4 = Ipv4(b1, b2, b3, b4);
+  else
+	is.setstate(std::ios_base::failbit);
+
+  return is;
+}
+
+}
+
---
+++ ipv4/ipv4.h
@@ -0,0 +1,74 @@
+#pragma once
+
+#include <array>
+
+namespace ipv4 {
+
+class Ipv4 {
+ public:
+  using IpByte = unsigned char;
+
+  constexpr Ipv4() : data_{{0}} {}
+  constexpr Ipv4(IpByte first, IpByte second, IpByte third, IpByte fought) :
+	  data_{first, second, third, fought} {}
+  explicit constexpr Ipv4(unsigned long ip) :
+	  data_{static_cast<IpByte>((ip >> 24) & 0xFF),
+			static_cast<IpByte>((ip >> 16) & 0xFF),
+			static_cast<IpByte>((ip >> 8) & 0xFF),
+			static_cast<IpByte>(ip & 0xFF)} {}
+
+  constexpr Ipv4(const Ipv4 &other) noexcept : data_{other.data_} {}
+  constexpr Ipv4 &operator=(const Ipv4 &other) noexcept {
+	data_ = other.data_;
+	return *this;
+  }
+
+  std::string to_string() const;
+  constexpr unsigned long to_ulong() const noexcept {
+	return (static_cast<unsigned long>(data_[0]) << 24) | (static_cast<unsigned long>(data_[1]) << 16) |
+		(static_cast<unsigned long>(data_[2]) << 8) | static_cast<unsigned long>(data_[3]);
+  }
+
+  constexpr Ipv4 &operator++() {
+	*this = Ipv4(1 + to_ulong());
+	return *this;
+  }
+
+  Ipv4 operator++(int) {
+	Ipv4 result(*this);
+	++(*this);
+	return result;
+  }
+
+  friend std::ostream &operator<<(std::ostream &os, const Ipv4 &ipv_4);
+  friend std::istream &operator>>(std::istream &is, Ipv4 &ipv_4);
+
+  friend bool operator==(const Ipv4 &left, const Ipv4 &right) noexcept {
+	return left.data_ == right.data_;
+  }
+
+  friend bool operator!=(const Ipv4 &left, const Ipv4 &right) noexcept {
+	return !(left == right);
+  }
+
+  friend constexpr bool operator<(const Ipv4 &left, const Ipv4 &right) noexcept {
+	return left.to_ulong() < right.to_ulong();
+  }
+
+  friend constexpr bool operator>(const Ipv4 &left, const Ipv4 &right) noexcept {
+	return right < left;
+  }
+
+  friend constexpr bool operator<=(const Ipv4 &left, const Ipv4 &right) noexcept {
+	return !(left > right);
+  }
+
+  friend constexpr bool operator>=(const Ipv4 &left, const Ipv4 &right) noexcept {
+	return !(left < right);
+  }
+
+ private:
+  std::array<IpByte, 4> data_;
+};
+
+}
