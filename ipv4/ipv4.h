a8f8866c48a151dce84ad2120e67a89d5bf3f994
Lebedev Roman <balakovo_roman@mail.ru>
2022-04-10 13:34:21 +0300

    Refactored class Ipv4
    Hiding the internal implementation.

--- ipv4/ipv4.h
+++ ipv4/ipv4.h
@@ -8,14 +8,8 @@ class Ipv4 {
  public:
   using IpByte = unsigned char;
 
-  constexpr Ipv4() : data_{{0}} {}
-  constexpr Ipv4(IpByte first, IpByte second, IpByte third, IpByte fought) :
+  constexpr Ipv4(IpByte first = 0, IpByte second = 0, IpByte third = 0, IpByte fought = 0) :
 	  data_{first, second, third, fought} {}
-  explicit constexpr Ipv4(unsigned long ip) :
-	  data_{static_cast<IpByte>((ip >> 24) & 0xFF),
-			static_cast<IpByte>((ip >> 16) & 0xFF),
-			static_cast<IpByte>((ip >> 8) & 0xFF),
-			static_cast<IpByte>(ip & 0xFF)} {}
 
   constexpr Ipv4(const Ipv4 &other) noexcept = default;
   constexpr Ipv4 &operator=(const Ipv4 &other) noexcept = default;
@@ -26,7 +20,7 @@ class Ipv4 {
 		(static_cast<unsigned long>(data_[2]) << 8) | static_cast<unsigned long>(data_[3]);
   }
 
-  constexpr Ipv4 &operator++() {
+  Ipv4 &operator++() {
 	*this = Ipv4(1 + to_ulong());
 	return *this;
   }
@@ -65,6 +59,17 @@ class Ipv4 {
   }
 
  private:
+  explicit constexpr Ipv4(unsigned long ip) :
+	  data_{static_cast<IpByte>((ip >> 24) & 0xFF),
+			static_cast<IpByte>((ip >> 16) & 0xFF),
+			static_cast<IpByte>((ip >> 8) & 0xFF),
+			static_cast<IpByte>(ip & 0xFF)} {}
+
+  constexpr unsigned long to_ulong() const noexcept {
+	return (static_cast<unsigned long>(data_[0]) << 24) | (static_cast<unsigned long>(data_[1]) << 16) |
+		(static_cast<unsigned long>(data_[2]) << 8) | static_cast<unsigned long>(data_[3]);
+  }
+
   std::array<IpByte, 4> data_;
 };
 
